b135e6a6650e61383eba4da3b1bd491ce12c812f
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2022-10-05 23:16:01 +0700

    helo

---
+++ docker_postgres_init.sql
@@ -0,0 +1,36 @@
+ALTER USER postgres PASSWORD '123456';
+CREATE DATABASE mhmarketaccount
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE DATABASE mhmarketrental
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE DATABASE mhmarketmarket
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE DATABASE mhmarketconfig
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
